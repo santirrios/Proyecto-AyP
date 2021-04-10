7903af245db258d666e384e4897c161087211377
santirrios <80852177+santirrios@users.noreply.github.com>
2021-04-10 18:23:15 -0500

    calcula meses en años

---
+++ calcula meses en años.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+int main(){
+	float anos,resultado;
+	
+	cout<<"ingrse el numero de años---->";
+	cin>>anos;
+	
+	resultado=(anos*12);
+	
+	cout<<"el numero de meses es---->"<<resultado;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
